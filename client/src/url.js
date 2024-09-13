fd49e99d7bfd6286a7ee75b13b1d877aa032bf80
SRIJANANI-27 <srija.janani2003@gmail.com>
2024-09-13 18:58:19 +0530

    url

--- client/src/url.js
+++ client/src/url.js
@@ -1,2 +1,6 @@
-export const baseurl = `https://balance-buddy.onrender.com`;
+// export const baseurl = `https://balance-buddy.onrender.com`;
 // export const baseurl ='http://localhost:4000'
+// url.js
+export const baseurl = process.env.NODE_ENV === 'production'
+  ? `https://balance-buddy.onrender.com`
+  : `http://localhost:4000`;
